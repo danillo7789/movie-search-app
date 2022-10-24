b75bbf779f9e3934a380c498108da83e50e7c0e3
daniel bolajoko <dhrey@bolajoko.com>
2022-10-24 18:10:29 +0100

    movie app built through youtube

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=904f401f';
+
+const App = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  const [searchInput, setSearchInput] = useState('');
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search)
+  };
+
+  useEffect(() => {
+    //passing in action as title
+    searchMovies('war')
+  }, []);
+
+  return (
+    <div className='app'>
+      <h1>MovieHouse</h1>
+
+      <div className='search'>
+        <input
+          value={searchInput}
+          placeholder='Search for Movies'
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='Search'
+          //passing in a new title to search "searchInput"
+          onClick={() => searchMovies(searchInput)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie, index) => (
+            <MovieCard movie={movie}
+              key={index} />
+          ))}
+        </div>) :
+        (
+          <div className='empty'>
+            <h2>No movies found</h2>
+          </div>
+        )}
+
+    </div>
+  );
+}
+
+export default App;
